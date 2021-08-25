1826a9356ca28bd54e1a4e7080686c2aae54b54f
Balcoder <balcode.software@gmail.com>
2021-08-25 11:21:19 -0500

    Add Splash_screen

---
+++ lib/ui/home/splash_screen.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  SplashScreenState createState() => new SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  var _visible = true;
+
+  late AnimationController animationController;
+  late Animation<double> animation;
+
+  startTime() async {
+    var _duration = new Duration(seconds: 3);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/OnBoardingScreen');
+    // Navigator.of(context).pushReplacementNamed('/RootService');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = new AnimationController(
+        vsync: this, duration: new Duration(seconds: 2));
+    animation =
+        new CurvedAnimation(parent: animationController, curve: Curves.easeOut);
+
+    animation.addListener(() => this.setState(() {}));
+    animationController.forward();
+
+    setState(() {
+      _visible = !_visible;
+    });
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 30.0),
+                child: SvgPicture.asset(
+                  "assets/images/powered_by_Balcoder.svg",
+                  height: 20.0,
+                  width: 149.0,
+                  fit: BoxFit.scaleDown,
+                ),
+              )
+            ],
+          ),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new SvgPicture.asset(
+                'assets/images/splash_screen.svg',
+                width: animation.value * 374,
+                height: animation.value * 374,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
